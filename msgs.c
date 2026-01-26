076133a561fadbbbe72b3e7c92d924a245c8056c
Theodor Wase <theodor.wase@axians.com>
2026-01-26 05:10:59 -0800

    Change msgs to opt in

--- msgs.c
+++ msgs.c
@@ -178,8 +178,8 @@ void msgs(void)
 
 	if (debug > 1) mrlog("msgs(%p, %d)", b, n);
 
-	if (get_option("no_msgs", 0)) {
-		mrsend(mrmachine, "msgs", "clear", "option no_msgs\n");
+	if (!get_option("enable_msgs", 0)) {
+		mrsend(mrmachine, "msgs", "clear", "option enable_msgs not set\n");
 		return;
 	}
 
